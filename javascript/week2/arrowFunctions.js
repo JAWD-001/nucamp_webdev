43cc827e2ec4d4c73cd9b727a8d073202a5b644c
James Dycus <dycus.j.jd@gmail.com>
2023-08-05 09:57:35 -0400

    checkGuess function to challengeExercise

---
+++ javascript/week2/arrowFunctions.js
@@ -0,0 +1,38 @@
+//function review
+
+function sayHello(userName) {
+  console.log(`Hello, ${userName}!`);
+}
+sayHello("John");
+
+function getPizza(size, nToppings) {
+  let cost = 10;
+  if (size === "medium") {
+    cost += 4;
+  } else if (size === "large") {
+    cost += 8;
+  }
+  cost += nToppings;
+  return cost;
+}
+
+let pizzaSize = "large";
+let numToppings = 2;
+let cost = getPizza(pizzaSize, numToppings);
+console.log(`The cost is: ${cost}`);
+
+//arrow functions added to JS in ES6, more concise and don't have a this binding
+
+const adder = (a, b) => a + b;
+console.log(adder(1, 2));
+
+//to use multiline function body, we must use curly braces and return statement.
+const adder2 = (a, b) => {
+  const c = a + b;
+  return c;
+};
+console.log(adder2(2, 3));
+
+//if single parameter, then can omit parathesis
+const double = (a) => a * 2;
+console.log(double(2));
---
+++ javascript/week2/challenge-exercise/challengeExercise.js
@@ -0,0 +1,34 @@
+function runGame() {
+  const targetNum = Math.floor(Math.random() * 100) + 1;
+  let correct = false;
+  let numTries = 0;
+  let guessNumber = 0;
+  let guessString = "";
+  do {
+    guessString = prompt(
+      "I am thinking of a number from 1 to 100, what is it?"
+    );
+    guessNumber = Number(guessString);
+    numTries++;
+    correct = checkGuess(guessNumber, targetNum);
+    alert(guessNumber);
+  } while (!correct);
+}
+
+function checkGuess(guessNumber, targetNum) {
+  let correct = false;
+  if (isNaN(guessNumber)) {
+    alert("Please enter a number between 1 - 100");
+  } else if (guessNumber < 1 || guessNumber > 100) {
+    alert(
+      "Your number is outside of the range, please guess a number between 1 and 100"
+    );
+  } else if (guessNumber > targetNum) {
+    alert("Your number is greater than the number");
+  } else if (guessNumber < targetNum) {
+    alert("Your number is less than the number");
+  } else {
+    correct = true;
+  }
+  return correct;
+}
