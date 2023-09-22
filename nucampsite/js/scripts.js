810780ceefc2a25a8551e650aeb78778434cc157
James Dycus <dycus.j.jd@gmail.com>
2023-09-22 10:19:04 -0400

    made js code more concise for nucampsite

--- nucampsite/js/scripts.js
+++ nucampsite/js/scripts.js
@@ -1,35 +1,38 @@
 console.log("javascript connected!");
 
 const carousel = new bootstrap.Carousel("#homeCarousel", {
-  interval: 2000,
+  interval: 3000,
   pause: false,
 });
 
-const carouselButton = document.getElementById("carouselButton");
-const faIcon = document.getElementById("faButton");
+
+const carouselButton = document.querySelector("#carouselButton");
+const faIcon = document.querySelector("#faButton");
+
 
 carouselButton.addEventListener("click", function () {
-  if (faIcon.classList.contains("fa-pause")) {
-    faIcon.classList.remove("fa-pause");
-    faIcon.classList.add("fa-play");
+  if (faIcon.classList.contains('fa-pause')){
+    faIcon.classList.remove('fa-pause');
+    faIcon.classList.add('fa-play');
     carousel.pause();
   } else {
-    faIcon.classList.remove("fa-play");
-    faIcon.classList.add("fa-pause");
+    faIcon.classList.remove('fa-play');
+    faIcon.classList.add('fa-pause');
     carousel.cycle();
   }
 });
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById("carouselPause");
-carouselPause.addEventListener("click", function () {
-  console.log("pausing the carousel");
-  carousel.pause();
-});
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById("carouselPlay");
-carouselPlay.addEventListener("click", function () {
-  console.log("cycle the carousel");
+const carouselPause = document.querySelector('#carouselPause');
+carouselPause.addEventListener('click', function () {
+  console.log('pausing carousel');
+  carousel.pause();
+})
+
+const carouselPlay = document.querySelector('#carouselPlay');
+carouselPlay.addEventListener('click', function() {
+  console.log('playing carousel');
   carousel.cycle();
-});
+}
+)
+
