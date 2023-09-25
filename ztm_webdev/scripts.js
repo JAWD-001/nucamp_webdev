3345d9c91d599dab52189fab447d386c9a5214de
James Dycus <dycus.j.jd@gmail.com>
2023-09-25 09:36:30 -0400

    added delete and strikethrough events and event handlers.

--- ztm_webdev/scripts.js
+++ ztm_webdev/scripts.js
@@ -10,11 +10,12 @@ let elementQueryAll = document.querySelectorAll("li,h1");
 // will select all of the li items and also the h1 element
 
 let itemInput = document.getElementById("item-input");
-
 let button = document.getElementById("enter");
-
 let list = document.querySelector("ul");
+let item = document.querySelector('li');
+let deleteBtns = document.querySelectorAll(".delete-btn");
 
+/*
 button.addEventListener("click", function () {
   if (itemInput.value.length > 0) {
     let newLi = document.createElement("li");
@@ -32,13 +33,88 @@ itemInput.addEventListener("keydown", function (event) {
     itemInput.value = "";
   }
 });
+*/
 
-//refactoring above code
+//REFACTORED FROM ABOVE
 
-button.addEventListener("mouseenter", function () {
-  console.log("MOUSE!");
+/*
+function inputLength (){
+  return itemInput.value.length();
+}
+
+function createLiElement (){
+  let newLi = document.createElement("li");
+  newLi.appendChild(document.createTextNode(itemInput.value));
+  list.appendChild(newLi);
+  itemInput.value = "";
+}
+
+button.addEventListener("click", function () {
+  if (inputLength() > 0) {
+    createLiElement();
+  }
 });
 
-button.addEventListener("mouseleave", function () {
-  console.log("MOUSE GONE!");
+
+itemInput.addEventListener("keydown", function (event) {
+  if (inputLength() > 0 && event.key === "Enter") {
+    createLiElement();
+  }
 });
+*/
+
+//REFACTORED YET AGAIN...
+
+function inputLength (){
+  return itemInput.value.length;
+}
+
+
+function createLiElement (){
+  let newLi = document.createElement("li");
+  newLi.classList.add('done');
+  newLi.appendChild(document.createTextNode(itemInput.value));
+  list.appendChild(newLi);
+  itemInput.value = "";
+  createBtnElement(newLi);
+}
+
+
+function createBtnElement(newLi){
+  let newBtn = document.createElement('button');
+  newBtn.classList.add('delete-btn');
+  newBtn.style.marginLeft = '10px';
+  newBtn.appendChild(document.createTextNode('Delete Item'));
+  newLi.appendChild(newBtn);
+}
+
+
+function addLiAfterClick(){
+  if (inputLength() > 0) {
+    createLiElement();
+  }
+}
+
+
+function addLiAfterEnter(event){
+  if (inputLength() > 0 && event.key === "Enter"){
+    createLiElement();
+  }
+}
+
+
+function liItemStatus(event) {
+  if (event.target.classList.contains('delete-btn')) {
+    event.target.parentElement.remove();
+  } 
+  if (event.target.tagName === 'LI'){
+  event.target.classList.toggle('done')
+  }
+}
+
+itemInput.addEventListener('keypress', addLiAfterEnter);
+
+button.addEventListener("click", addLiAfterClick);
+
+list.addEventListener('click', liItemStatus);
+
