24fc5c52a5263eb9a0b762c7438aaadf21e57447
James Dycus <dycus.j.jd@gmail.com>
2023-10-20 12:14:18 -0400

    added conactpage and homepage to pages.

--- react/nucampsite/src/App.js
+++ react/nucampsite/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <CampsitesDirectoryPage/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer/>
     </div>
   );
