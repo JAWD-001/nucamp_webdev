131c189531da74ad49eff3fa17b185482a64e8e0
James Dycus <dycus.j.jd@gmail.com>
2023-11-15 12:09:20 -0500

    added directory component

--- ztm_webdev/react/crwn-clothing/src/components/category-directory/category-directory.component.jsx
+++ ztm_webdev/react/crwn-clothing/src/components/category-directory/category-directory.component.jsx
@@ -1,7 +1,15 @@
 import './category-directory.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
 
-const CategoryDirectory =  () => {
-
+const CategoryDirectory =  ({categories}) => {
+	  
+	return(
+		<div className='categories-container'>
+		{categories.map((category) => (
+		  <CategoryItem key={category.id} category={category}/>
+		))}
+	  </div>
+	);
 };
 
 export default CategoryDirectory;
